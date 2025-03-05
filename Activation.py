f58f1807df4c943f64989586053bd5d982be5675
alexwox <alexander.woxstrom@gmail.com>
2025-03-05 01:34:33 +0100

    activation functions, relu and softmax

---
+++ Activation.py
@@ -0,0 +1,17 @@
+import numpy as np
+from typing import List, Tuple
+
+class Activation:
+    @staticmethod
+    def relu(x: np.ndarray) -> np.ndarray:
+        return np.maximum(0, x)
+    
+    @staticmethod
+    def relu_derivative(x: np.ndarray) -> np.ndarray:
+        return (x > 0).astype(float)
+    
+    @staticmethod
+    def softmax(x: np.ndarray) -> np.ndarray:
+        exp_x = np.exp(x-np.max(x, axis=1, keepdims=True))
+        return exp_x / np.sum(exp_x, axis=1, keepdims=True)
+
